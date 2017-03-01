fbbd1148b13d12f54839a7a534392a6e7277c6f7
Andres Monroy <monroy@adobe.com>
2017-02-28 17:07:43 -0700

    init

---
+++ handler.js
@@ -0,0 +1,27 @@
+var lib = require('./lib/hello');
+
+module.exports.hello = function (context, req) {
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    if (req.query.name || (req.body && req.body.name)) {
+        res = {
+            // status: 200, /* Defaults to 200 */
+            body: lib.hello(req.query.name || req.body.name)
+        };
+    }
+    else {
+        res = {
+            status: 400,
+            body: "Please pass a name on the query string or in the request body"
+        };
+    }
+    context.done(null, res);
+};
+
+module.exports.helloQueue = function (context, queueItem) {
+    context.log('JavaScript queue trigger function processed work item', queueItem);
+    context.bindings.blobOut = {
+        "hello": "world"
+    }
+    context.done();
+};
